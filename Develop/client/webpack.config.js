5d737c57a8ad219aa7efd57952df7fd94bed6012
JJGAvery <116852587+JJGAvery@users.noreply.github.com>
2023-05-25 19:30:35 -0600

    added and configured workbox plugins for service worker and manifest file, added css loaders and babel to webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -13,18 +13,56 @@ module.exports = () => {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+
     output: {
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, 'dist')
     },
-    plugins: [
-      
-    ],
 
-    module: {
-      rules: [
-        
-      ],
-    },
+    plugins: [
+      new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'J.A.T.E.'
+      }),   
+    
+    new WebpackPwaManifest({
+      name: 'J.A.T.E.',
+      description: 'Just Another Text Editor',
+      background_color: '#ffffff',
+      fingerprints: false,
+      publicPath: './',
+      inject: false,
+      icons: [
+        {
+          src: path.resolve('./src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        }
+      ]
+    }),
+
+    new InjectManifest({ 
+      swSrc: './src-sw.js', 
+      swDest: 'service-worker.js',
+    }),
+  ],
+    module:{
+      rules:[{
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-object-rest-spread'],
+          },
+        }
+      }
+      ]
+    }
   };
 };
